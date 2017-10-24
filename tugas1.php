4e0a6b19e10b3efee1556d54ff7de63252f56371
161710157 <161710157@smkassalaambandung.sch.id>
2017-10-24 22:22:10 +0700

    latihan ke 5 saya

---
+++ tugas1.php
@@ -0,0 +1,12 @@
+<?php
+
+class kucing{
+public $suara ="meong meong meong";
+public $berat =30;
+
+}
+$kucing = new kucing;
+echo "berat kucing :".$kucing->berat. "<br>";
+echo "bunyi kucing :".$kucing->suara."<br>";
+
+?>
